b240920eb86f2fd4b5e3dc9b727d5d19fe9fe8f6
ferdinando99 <f.geografo@reply.it>
2024-07-04 16:19:14 +0200

    progress on form

--- src/app/ui/result/result.component.ts
+++ src/app/ui/result/result.component.ts
@@ -6,14 +6,33 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <section class="result">
+      @if(true) {
+      <h2 class="text text--lg">Your results</h2>
+      <p class="text text--sm">
+        Your results are shown below based on the information you provided. To
+        adjust the results, edit the form and click "calculate repayments"
+        again.
+      </p>
+
+      <div class="card">
+        <div class="card__header">
+          <h4 class="text text--sm">Your monthly repayments</h4>
+          <p class="text text--xl">£1,797.74</p>
+        </div>
+
+        <div class="card__body">
+          <h4 class="text text--sm">Total you'll repay over the term</h4>
+          <p class="text text--lg">£539,322.94</p>
+        </div>
+      </div>
+      } @else {
       <img src="images/illustration-empty.svg" alt="Illustration" />
-
       <h2 class="text text--lg">Results shown here</h2>
-
       <p class="text text--sm">
         Complete the form and click “calculate repayments” to see what your
         monthly repayments would be.
       </p>
+      }
     </section>
   `,
   styles: `
@@ -39,6 +58,27 @@ import { Component } from '@angular/core';
 
       .text--sm {
         color: var(--slate-200);
+      }
+    }
+
+    .card {
+      padding: var(--spacing-400);
+      background: var(--black-100);
+      border-radius: 8px;
+      box-shadow: inset 0 4px 0 0 var(--lime-100);
+
+      &__header, &__body {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+      }
+
+      &__header {
+        .text--xl { color: var(--lime-100); }
+      }
+
+      &__body {
+
       }
     }
   `,
