0bb8e18656dccd976557f13cba594cfccb781d06
FerdinandoGeografo <ferdinandogeografo@live.it>
2024-07-06 17:14:47 +0200

    responsive for tablet and desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,16 @@ import { ResultComponent } from './ui/result/result.component';
     </main>
   `,
   styles: `
+    @use "../../public/scss/_query-mixin.scss" as mixin;
+
     :host {
-      max-width: 100.8rem;
+      max-width: 108.8rem;
       margin: 0 auto;
-      padding: 4.8rem 0;
+      padding: var(--spacing-600) var(--spacing-500);
+
+      @include mixin.respond(phone) {
+        padding: 0;
+      }
     }
 
     .main {
@@ -26,6 +32,14 @@ import { ResultComponent } from './ui/result/result.component';
       border-radius: 2.4rem;
       overflow: hidden;
       filter: drop-shadow(0 3.2rem 6.4rem var(--slate-600));
+
+      @include mixin.respond(tablet) {
+        grid-template-columns: 1fr;
+      }
+
+      @include mixin.respond(phone) {
+        border-radius: 0;
+      }
     }
   `,
 })
